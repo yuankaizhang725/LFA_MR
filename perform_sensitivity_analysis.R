b0873d73608765cedb045f95d1077ac60c9fa846
YuankaiZhang <77945918+yuankaizhang725@users.noreply.github.com>
2025-02-07 19:39:56 -0500

    perform_sensitivity_analysis.R

--- perform_sensitivity_analysis.R
+++ perform_sensitivity_analysis.R
@@ -1,11 +1,11 @@
 #' Perform sensitivity analysis for MVMR_BFA
-#' @param exposure_data Matrix of exposure variables
+#' @param exposure_beta Matrix of exposure variables
 #' @param outcome_beta Vector of outcome betas
 #' @param outcome_se Vector of outcome standard errors
 #' @param Kmax_range Vector of Kmax values to try
 #' @param other parameters as in MVMR_BFA
 #' @return List of results for different parameter settings
-perform_sensitivity_analysis <- function(exposure_data, outcome_beta, outcome_se,
+perform_sensitivity_analysis <- function(exposure_beta, outcome_beta, outcome_se,
                                          Kmax_range = 3:8, 
                                          models = c("fixed", "random"),
                                          Nid_range = 2:3) {
@@ -17,7 +17,7 @@ perform_sensitivity_analysis <- function(exposure_data, outcome_beta, outcome_se
         key <- paste("Kmax", Kmax, "model", model, "Nid", Nid, sep="_")
         
         results[[key]] <- try({
-          MVMR_BFA(exposure_data = exposure_data,
+          MVMR_BFA(exposure_beta = exposure_beta,
                    outcome_beta = outcome_beta,
                    outcome_se = outcome_se,
                    Kmax = Kmax,
